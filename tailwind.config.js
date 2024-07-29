53b380084ffeb8fd13f22f68af2fdc79772a8292
manishkumar632 <manishmu632@gmail.com>
2024-07-29 21:53:01 +0530

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./app/templates/**/*.{html,js}",
+        "./app/templates/includes/**/*.{html,js}"
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "radial-circle":
+                    "radial-gradient(circle at center center, #0F2C41,  #0F172A, #0F172A)"
+            },
+            colors: {
+                color1: "#0F172A",
+                color2: "#D8E6FB",
+                color3: "orange"
+            },
+            fontFamily: {
+                ubuntu: ["Ubuntu", "sans-serif"]
+            }
+        },
+        plugins: []
+    }
+};
