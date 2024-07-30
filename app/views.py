2509056a2efea98410d183a68b1ab167c8a93d35
Niraj Kumar <niraj@example.com>
2024-07-30 21:39:15 +0530

    commited

--- app/views.py
+++ app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
